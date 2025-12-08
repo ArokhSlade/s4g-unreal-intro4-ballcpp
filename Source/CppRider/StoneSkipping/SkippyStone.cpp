e90a953cd681af904687c7dabceec28d6b38b764
Arokh Slade <arokhslade@gmail.com>
2025-12-08 13:39:16 +0100

    basic skippy stone c++ code for boost and lean sideways (compiles, untested)

--- Source/CppRider/StoneSkipping/SkippyStone.cpp
+++ Source/CppRider/StoneSkipping/SkippyStone.cpp
@@ -1,7 +1,10 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "StoneSkipping/SkippyStone.h"
+#include "SkippyStone.h"
+
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
 
 // Sets default values
 ASkippyStone::ASkippyStone()
@@ -9,6 +12,16 @@ ASkippyStone::ASkippyStone()
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>("Mesh");
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>("SpringArm");
+	Camera = CreateDefaultSubobject<UCameraComponent>("Camera");
+
+	RootComponent = Mesh;
+	SpringArm->SetupAttachment(Mesh);
+	Camera->SetupAttachment(SpringArm);
+
+	LeanSidewaysStrength = 100'000.0f;
+	Mesh->SetSimulatePhysics(true);
 }
 
 // Called when the game starts or when spawned
@@ -23,12 +36,47 @@ void ASkippyStone::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	if (SkippyStoneInput.bBoost)
+	{
+		FVector Impulse { 0.f, 0.f, BoostStrength };
+		Boost(Impulse);
+	}
+
+	FVector LeanSidewaysForce = FVector{ 0.f, SkippyStoneInput.LeanSideways * LeanSidewaysStrength, 0.f };
+	LeanSideways(LeanSidewaysForce);
+
 }
 
 // Called to bind functionality to input
 void ASkippyStone::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
+	InputComponent->BindAxis("MoveRight", this, &ASkippyStone::OnLeanSidewaysInput);
+	InputComponent->BindAction("Jump", IE_Pressed, this, &ASkippyStone::OnBoostPressedInput);
+	InputComponent->BindAction("Jump", IE_Released, this, &ASkippyStone::OnBoostReleasedInput);
 }
 
+void ASkippyStone::OnBoostPressedInput()
+{
+	SkippyStoneInput.bBoost = true;
+}
+
+void ASkippyStone::OnBoostReleasedInput()
+{
+	SkippyStoneInput.bBoost = false;
+}
+
+void ASkippyStone::OnLeanSidewaysInput(float AxisValue)
+{
+	SkippyStoneInput.LeanSideways = AxisValue;
+}
+
+void ASkippyStone::Boost_Implementation(FVector Impulse)
+{
+	Mesh->AddImpulse(Impulse);
+}
+
+void ASkippyStone::LeanSideways_Implementation(FVector Force)
+{
+	Mesh->AddForce(Force);
+}
--- Source/CppRider/StoneSkipping/SkippyStone.h
+++ Source/CppRider/StoneSkipping/SkippyStone.h
@@ -6,6 +6,23 @@
 #include "GameFramework/Pawn.h"
 #include "SkippyStone.generated.h"
 
+class USpringArmComponent;
+class UCameraComponent;
+
+USTRUCT(BlueprintType)
+struct FSkippyStoneInput
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Skippy Stone|Input")
+	float LeanSideways;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Skippy Stone|Input")
+	uint32 bBoost : 1;
+};
+
+
 UCLASS()
 class CPPRIDER_API ASkippyStone : public APawn
 {
@@ -19,11 +36,41 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
+public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+	void OnBoostPressedInput();
+	void OnBoostReleasedInput();
+	void OnLeanSidewaysInput(float AxisValue);
+
+	UFUNCTION(BlueprintNativeEvent, Category = SkippyStone)
+	void Boost(FVector Impulse);
+
+	UFUNCTION(BlueprintNativeEvent, Category = SkippyStone)
+	void LeanSideways(FVector Force);
+
+
+protected:	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Skippy Stone")
+	UStaticMeshComponent* Mesh;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Skippy Stone")
+	USpringArmComponent* SpringArm;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Skippy Stone")
+	UCameraComponent* Camera;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Skippy Stone|Input")
+	FSkippyStoneInput SkippyStoneInput;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Skippy Stone")
+	float LeanSidewaysStrength;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Skippy Stone")
+	float BoostStrength;
+	
 };
